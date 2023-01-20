5b4b9d2aabf51c387448e0a2a19d89c0c9898d03
Tbownt <andresjose65@hotmail.com>
2023-01-20 18:42:35 -0400

    footer estilado

--- my-app/src/Componets/Details/Details.jsx
+++ my-app/src/Componets/Details/Details.jsx
@@ -1,45 +1,46 @@
-
 // import { useEffect } from "react";
 // import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 // import { useParams } from "react-router-dom";
 // import { petDetails } from "../../Redux/Actions/index";
 
 const Details = () => {
-	// listo para conectar con el back
-	// const dispatch = useDispatch();
-	// const { paramsId } = useParams();
-	// const Detail = useSelector((state) => state.Detail);
+  // listo para conectar con el back
+  // const dispatch = useDispatch();
+  // const { paramsId } = useParams();
+  // const Detail = useSelector((state) => state.Detail);
 
-	// useEffect(() => {
-	// 	dispatch(petDetails(paramsId));
-	// }, [dispatch]);
+  // useEffect(() => {
+  // 	dispatch(petDetails(paramsId));
+  // }, [dispatch]);
 
-	return (
-		<>
-			<Navbar />
-			<div>
-				<div>
-					<img
-						src={
-							"https://www.elpaisdelosjovenes.com/wp-content/uploads/2015/05/03_02.jpg"
-						}
-						alt={"dale"}
-					/>
-					<h1> {"Perro"}</h1>
-					<h2>Sexo: {"M"}</h2>
-				</div>
-				<div>
-					<h3>
-						Detalle:{"Tiene el pelo cafe  , ademas tiene una patica mala"}
-					</h3>
+  return (
+    <>
+      <Navbar />
+      <div>
+        <div>
+          <img
+            src={
+              "https://www.elpaisdelosjovenes.com/wp-content/uploads/2015/05/03_02.jpg"
+            }
+            alt={"dale"}
+          />
+          <h1> {"Perro"}</h1>
+          <h2>Sexo: {"M"}</h2>
+        </div>
+        <div>
+          <h3>
+            Detalle:{"Tiene el pelo cafe  , ademas tiene una patica mala"}
+          </h3>
 
-					<p>Area: {"Se encuentra en robledo cerca a la quintana"}</p>
-					<p>species: {"no"}</p>
-				</div>
-			</div>
-		</>
-	);
+          <p>Area: {"Se encuentra en robledo cerca a la quintana"}</p>
+          <p>species: {"no"}</p>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Details;
@@ -56,4 +57,3 @@ export default Details;
 // 					<p>Area: {Detail.area}</p>
 // 					<p>species: {Detail.species}</p>
 // 				</div>
-
