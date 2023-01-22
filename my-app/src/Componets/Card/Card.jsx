188630512424fe15e41ba43a6988e431fc58c9ce
Tbownt <andresjose65@hotmail.com>
2023-01-21 22:24:52 -0400

    landing estilada - refactor en comentarios

--- my-app/src/Componets/Card/Card.jsx
+++ my-app/src/Componets/Card/Card.jsx
@@ -1,35 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./Card.css";
 
-
 const Card = ({ data: { size, img, sex } }) => {
-	return (
-		<div className="card2">
-			<div className="face front">
-				<img src={img} alt="img not found" />
-				<h3>{size}</h3> {/* size deberia ser el tamaño, type/specie si es gato o perro */}
-			</div>
-			<div className="face back">
-				<h3>Sex: {sex}</h3>
-				<p>Lorem ipsum dolor sit.</p>
-				<p>Lorem ipsum dolor sit.</p>
-				<p>Lorem ipsum dolor sit.</p>
-				<div className="link2">
-					<a href="#">Click for details</a>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="card2">
+      <div className="face front">
+        <img src={img} alt="img not found" />
+        <h3>{size}</h3>{" "}
+        {/* size deberia ser el tamaño, type/specie si es gato o perro */}
+      </div>
+      <div className="face back">
+        <h3>Sex: {sex}</h3>
+        <p>Lorem ipsum dolor sit.</p>
+        <p>Lorem ipsum dolor sit.</p>
+        <p>Lorem ipsum dolor sit.</p>
+        <div className="link2">
+          <a href="#">Click for details</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
 
-
-
-
 // ↓↓↓   Asi es como dejó Julian el codigo el 20/01/23   ↓↓↓
-{/* 
+/* 
 		<div className="cardContainer">
 			<p className="petName"> {size}</p>
 			<p className="petSex"> {sex}</p>
@@ -40,16 +37,11 @@ export default Card;
 				</div>
 			</div>
 		</div> 
-*/}
-
-
-
-
-
-
+*/
 
 // ↓↓↓↓    Asi es como Ale dejó el codigo jueves 19/01/23     ↓↓↓↓
-{/* 
+
+/* 
 		<div className="card">
 			<div className="TextCard">
 				<p className="TextCard">Tipo:{Tipo}</p>
@@ -57,4 +49,4 @@ export default Card;
 				<p className="TextCard">Nombre:{Nombre}</p>
 			</div>
 		</div> 
-*/}
+*/
--- my-app/src/Componets/LandingPage/LandingPage.jsx
+++ my-app/src/Componets/LandingPage/LandingPage.jsx
@@ -1,17 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/imagenes/logo_negro.png";
+// import logo2 from "../../assets/imagenes/logo_morado.png";
+import "./LandingPage.css";
+
+/* <div className="row justify-content-center">
+  <div className="col-12">
+    <h2>Welcome</h2>
+    <Link to="/home">
+      <button className="btn btn-primary">Ingresa</button>
+    </Link>
+  </div>
+  <div className="col-12">
+    <Link to="/login">
+      <button className="btn btn-primary">
+        Ingresa con tu usuario
+      </button>
+    </Link>
+  </div>
+</div>
+</div> */
 
 const LandingPage = () => {
   return (
-    <div>
-      <div className="Landing">
-        <h1 className="titleLanding">Welecome</h1>
-        <Link to="/home">
-          <button className="Button">Visitar</button>
-        </Link>
-        <Link to="/login">
-          <button className="Button">Ingresa con tu usuario</button>
-        </Link>
+    <div className="contenedor">
+      <img src={logo} alt="logo" width="500em" className="logo" />
+      <div className="container-box">
+        <div className="box">
+          <img src={logo} alt="logo" width="125em" name="logo" />
+          <label htmlFor="logo">
+            Donde ayudamos a nuestros amiguitos peludos
+          </label>
+          <Link to="/home">
+            <button className="btn btn-success">Ingresar</button>
+          </Link>
+          <Link to="/home">
+            <button className="btn btn-warning">
+              Ingresar sin registrarse
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
