d332303bb98b2313720aba0f6f634760e4167cdc
coderman47 <askinglucho@gmail.com>
2023-01-23 20:09:41 -0300

    Css: Estilo animado de numeros 404 en componente.Lucho

--- my-app/src/Componets/NotFound/NotFound.jsx
+++ my-app/src/Componets/NotFound/NotFound.jsx
@@ -2,23 +2,31 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
-	const navigate = useNavigate();
-	const redirect = () => setTimeout(() => navigate("/home"), 5000);
+  const navigate = useNavigate();
+  const redirect = () => setTimeout(() => navigate("/home"), 5000);
 
-	useEffect(() => {
-		redirect();
-	}, []);
-	return (
-		<>
-			<Navbar />
-			<div className="soft-not-found">
-				<h1>Route not found, redirecting to home in 5 seconds...</h1>
-			</div>
-			<Footer />
-		</>
-	);
+  useEffect(() => {
+    redirect();
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <div class="div-modified">
+          <h1 class="textDecorate"> ¡Ups! Lo siento, esta página no existe. Redireccionandote al Home en 5 seg...</h1>
+        <section class="error-container">
+          <span><span>4</span></span>
+          <span>0</span>
+          <span><span>4</span></span>
+        </section>
+      </div>
+      <div className="footerPlace"> {/* agregue */}
+      <Footer />
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
