09aeb2bda162d6bd0484b85d8a93152317cb646b
Tbownt <98657140+Tbownt@users.noreply.github.com>
2023-01-25 23:51:52 -0400

    Auth0 implementado

--- my-app/src/Componets/Home/Home.jsx
+++ my-app/src/Componets/Home/Home.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Slider from "../Slider/Slider";
 import Footer from "../Footer/Footer";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./Home.css";
 
 const Home = () => {
+  const { user } = useAuth0();
+  console.log(user);
   return (
     <>
       <Navbar />
