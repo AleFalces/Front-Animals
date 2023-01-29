a35bfe5b14830f82c5110c832ca8087c0ff05b77
maguialiaga <maguialiaga1@gmail.com>
2023-01-29 16:44:27 -0300

    estilosss

--- my-app/src/Componets/Footer/Footer.jsx
+++ my-app/src/Componets/Footer/Footer.jsx
@@ -1,34 +1,136 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+import {
+  Box,
+  Image,
+  chakra,
+  Container,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  VisuallyHidden,
+  // Input,
+  // IconButton,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import logo from "../../assets/imagenes/logo_negro.png";
-import "./Footer.css";
+import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
-const Footer = () => {
+const Logo = () => {
   return (
-    <div>
-      <footer className="footer">
-        <Container fluid>
-          <Row className="align-items-center p-3">
-            <Col className="navbar">
-              <div>
-                <img
-                  src={logo}
-                  alt="home"
-                  height="105em"
-                  onClick={() => window.scrollTo(0, 0)}
-                />
-              </div>
-            </Col>
-            <Col
-              size={12}
-              lg={6}
-              className="text-center text-sm-center text-md-end"
-            ></Col>
-          </Row>
-        </Container>
-      </footer>
-    </div>
+    <Box>
+      <Image
+        src={logo}
+        // alt="Dan Abramov"
+        boxSize="90px"
+        mx="1rem"
+        mt="1rem"
+        onClick={() => window.scrollTo(0, 0)}
+      />
+    </Box>
   );
 };
-export default Footer;
+
+const SocialButton = ({ children, label, href }) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
+      w={8}
+      h={8}
+      cursor={"pointer"}
+      as={"a"}
+      href={href}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
+      _hover={{
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+const ListHeader = ({ children }) => {
+  return (
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+export default function LargeWithNewsletter() {
+  return (
+    <Box
+      bg="brand.green.100"
+      color={useColorModeValue("gray.700", "gray.200")}
+      p={10}
+    >
+      <Container as={Stack} maxW={"6xl"} py={10}>
+        <SimpleGrid
+          templateColumns={{ sm: "1fr 1fr", md: "2fr 1fr 1fr 2fr" }}
+          spacing={5}
+        >
+          <Stack spacing={6}>
+            <Box>
+              <Logo color={useColorModeValue("gray.700", "white")} />
+            </Box>
+            <Text fontSize={"sm"}>© 2023 Buddy ONG. All rights reserved</Text>
+            <Stack direction={"row"} spacing={6}>
+              <SocialButton label={"Twitter"} href={"#"}>
+                <FaTwitter />
+              </SocialButton>
+              <SocialButton label={"YouTube"} href={"#"}>
+                <FaYoutube />
+              </SocialButton>
+              <SocialButton label={"Instagram"} href={"#"}>
+                <FaInstagram />
+              </SocialButton>
+            </Stack>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Company</ListHeader>
+            <Link href={"#"}>About us</Link>
+            <Link href={"#"}>Blog</Link>
+            <Link href={"#"}>Contact us</Link>
+            <Link href={"#"}>Pricing</Link>
+            <Link href={"#"}>Testimonials</Link>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Support</ListHeader>
+            <Link href={"#"}>Help Center</Link>
+            <Link href={"#"}>Terms of Service</Link>
+            <Link href={"#"}>Legal</Link>
+            <Link href={"#"}>Privacy Policy</Link>
+            <Link href={"#"}>Satus</Link>
+          </Stack>
+          {/* <Stack align={"flex-start"}>
+            <ListHeader>Stay up to date</ListHeader>
+            <Stack direction={"row"}>
+              <Input
+                placeholder={"Your email address"}
+                bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+                border={0}
+                _focus={{
+                  bg: "whiteAlpha.300",
+                }}
+              />
+              <IconButton
+                bg={useColorModeValue("green.400", "green.800")}
+                color={useColorModeValue("white", "gray.800")}
+                _hover={{
+                  bg: "green.600",
+                }}
+                aria-label="Subscribe"
+                icon={<BiMailSend />}
+              />
+            </Stack>
+          </Stack> */}
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
+}
