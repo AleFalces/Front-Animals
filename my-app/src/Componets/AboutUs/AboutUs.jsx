0935ac133142356cb802a6e744ac6e4259dabc82
maguialiaga <maguialiaga1@gmail.com>
2023-01-30 09:59:48 -0300

    cambios home

--- my-app/src/Componets/AboutUs/AboutUs.jsx
+++ my-app/src/Componets/AboutUs/AboutUs.jsx
@@ -1,73 +1,169 @@
 import React from "react";
+import {
+  Box,
+  Heading,
+  Container,
+  SimpleGrid,
+  Text,
+  Stack,
+  Flex,
+  HStack,
+  // Icon,
+  VStack,
+  // createIcon,
+} from "@chakra-ui/react";
+// import { CheckIcon } from "@chakra-ui/icons";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import img4 from "../../assets/imagenes/img4.jpg";
 
 const AboutUs = () => {
-	return (
-		<>
-			<div>
-				<Navbar />
-				<img className="d-block w-100 edit" src={img4} alt="First slide" />
-				<h1>¿Quienes Somos?</h1>
-				<p>
-					Somos organización sin fines de lucro liderada por un grupo de
-					voluntarios que buscan superar la situación de sobrepoblación,
-					abandono, crueldad e indiferencia que viven millones de animales en
-					nuestro país. (Buenos Aires/ Argentina). Propiciamos una actitud de
-					respeto hacia todas las especies, entendiendo que no son “cosas” para
-					ser utilizadas por el ser humano. Rechazamos todo tipo de explotación
-					animal, incluyendo su uso como vestimenta, comida, entretenimiento y
-					experimentación.
-				</p>
-				<h1>Nuestros objetivos</h1>
-				<ul>
-					• Luchar contra el abandono, el maltrato y el sufrimiento animal.
-				</ul>
-				<ul>
-					• Educar sobre el respeto por la vida de los animales a través de
-					charlas y talleres educativos en lugares públicos y privados.
-				</ul>
-				<ul>
-					• Organizar campañas de castración gratuitas y/o a bajo costo en las
-					zonas vulnerables donde el Estado está ausente y los perros y gatos se
-					reproducen sin control.
-				</ul>
-				<ul>
-					• Asistir a animales en situación de riesgo de muerte, brindarles la
-					atención médica necesaria para recuperarlos y encontrar familias
-					responsables para su adopción.
-				</ul>
-				<ul>
-					• Asesorar a la población respecto a cómo actuar en casos de maltrato
-					animal.
-				</ul>
+  return (
+    <>
+      <Navbar />
+      <Box h={"1700"} bg="brand.backgorund">
+        <Flex
+          w={"full"}
+          h={"90vh"}
+          backgroundImage={
+            "url(https://www.hogarmania.com/archivos/202011/cosas-donar-refugio-animales-4-XxXx80.jpg)"
+          }
+          backgroundSize={"cover"}
+          backgroundPosition={"center center"}
+        ></Flex>
+        <Container maxW={"3xl"}>
+          <Stack
+            as={Box}
+            textAlign={"center"}
+            spacing={{ base: 8, md: 14 }}
+            py={{ base: 20, md: 36 }}
+          >
+            <Heading
+              fontWeight={600}
+              fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+              lineHeight={"150%"}
+            >
+              Hola!
+              <br />
+              <Text as={"span"} color={"brand.orange"}>
+                Somos Buddy ONG
+              </Text>
+            </Heading>
+            <Text color={"gray.500"}>
+              Somos organización sin fines de lucro liderada por un grupo de
+              voluntarios que buscan superar la situación de sobrepoblación,
+              abandono, crueldad e indiferencia que viven millones de animales
+              en nuestro país. (Buenos Aires/ Argentina). Propiciamos una
+              actitud de respeto hacia todas las especies, entendiendo que no
+              son “cosas” para ser utilizadas por el ser humano. Rechazamos todo
+              tipo de explotación animal, incluyendo su uso como vestimenta,
+              comida, entretenimiento y experimentación.
+            </Text>
 
-				<h1>¿Cómo trabajamos?</h1>
-				<ul>
-					• La acción directa: asistir a animales abandonados en situación de
-					riesgo, promoviendo su adopción y tenencia por parte de hogares
-					responsables que estén en condiciones de brindarles albergue, atención
-					y afecto.
-				</ul>
-				<ul>
-					• La acción preventiva: fomentar entre el público general la necesidad
-					y la importancia de castrar machos y esterilizar las hembras antes del
-					primer celo y exigiendo a las autoridades la aplicación de las leyes
-					que los obligan a realizar campañas de castración masivas, gratuitas,
-					extendidas, sistemáticas y permanentes, como único medio humanitario,
-					sustentable y no eutanásico de control de la superpoblación animal.
-				</ul>
-				<ul>
-					• Las actividades asistenciales: rescatar animales en situación de
-					riesgo con el fin de recuperarlos y darles la posibilidad de formar
-					parte de una familia que le brinde lo que necesita.
-				</ul>
+            <Box p={4}>
+              <Stack
+                spacing={4}
+                as={Container}
+                maxW={"3xl"}
+                textAlign={"center"}
+              >
+                <Heading fontSize={"3xl"}>Nuestros Objetivos</Heading>
+              </Stack>
 
-				<Footer />
-			</div>
-		</>
-	);
+              <Container maxW={"6xl"} mt={10}>
+                <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+                  <HStack align={"top"}>
+                    <VStack align={"start"}>
+                      <Text fontWeight={600}></Text>
+                      <Text color={"gray.600"}>
+                        Educar sobre el respeto por la vida de los animales a
+                        través de charlas y talleres educativos en lugares
+                        públicos y privados.
+                      </Text>
+                    </VStack>
+                  </HStack>
+                  <HStack align={"top"}>
+                    <VStack align={"start"}>
+                      <Text fontWeight={600}></Text>
+                      <Text color={"gray.600"}>
+                        Asistir a animales en situación de riesgo de muerte,
+                        brindarles la atención médica necesaria para
+                        recuperarlos y encontrar familias responsables para su
+                        adopción.
+                      </Text>
+                    </VStack>
+                  </HStack>
+                  <HStack align={"top"}>
+                    <VStack align={"start"}>
+                      <Text fontWeight={600}></Text>
+                      <Text color={"gray.600"}>
+                        Luchar contra el abandono, el maltrato y el sufrimiento
+                        animal
+                      </Text>
+                    </VStack>
+                  </HStack>
+                  <HStack align={"top"}>
+                    <VStack align={"start"}>
+                      <Text fontWeight={600}></Text>
+                      <Text color={"gray.600"}>
+                        Organizar campañas de castración gratuitas y/o a bajo
+                        costo en las zonas vulnerables donde el Estado está
+                        ausente y los perros y gatos se reproducen sin control.
+                      </Text>
+                    </VStack>
+                  </HStack>
+                </SimpleGrid>
+              </Container>
+            </Box>
+          </Stack>
+        </Container>
+      </Box>
+      <Footer />
+    </>
+  );
 };
 
 export default AboutUs;
+
+//   <h1>Nuestros objetivos</h1>
+//   <ul>• Luchar contra el abandono, el maltrato y el sufrimiento animal.</ul>
+//   <ul>
+//     • Educar sobre el respeto por la vida de los animales a través de charlas y
+//     talleres educativos en lugares públicos y privados.
+//   </ul>
+//   <ul>
+//     • Organizar campañas de castración gratuitas y/o a bajo costo en las zonas
+//     vulnerables donde el Estado está ausente y los perros y gatos se reproducen
+//     sin control.
+//   </ul>
+//   <ul>
+//     • Asistir a animales en situación de riesgo de muerte, brindarles la
+//     atención médica necesaria para recuperarlos y encontrar familias
+//     responsables para su adopción.
+//   </ul>
+//   <ul>
+//     • Asesorar a la población respecto a cómo actuar en casos de maltrato
+//     animal.
+//   </ul>
+
+//   <h1>¿Cómo trabajamos?</h1>
+//   <ul>
+//     • La acción directa: asistir a animales abandonados en situación de riesgo,
+//     promoviendo su adopción y tenencia por parte de hogares responsables que
+//     estén en condiciones de brindarles albergue, atención y afecto.
+//   </ul>
+//   <ul>
+//     • La acción preventiva: fomentar entre el público general la necesidad y la
+//     importancia de castrar machos y esterilizar las hembras antes del primer
+//     celo y exigiendo a las autoridades la aplicación de las leyes que los
+//     obligan a realizar campañas de castración masivas, gratuitas, extendidas,
+//     sistemáticas y permanentes, como único medio humanitario, sustentable y no
+//     eutanásico de control de la superpoblación animal.
+//   </ul>
+//   <ul>
+//     • Las actividades asistenciales: rescatar animales en situación de riesgo
+//     con el fin de recuperarlos y darles la posibilidad de formar parte de una
+//     familia que le brinde lo que necesita.
+//   </ul>
+
+//   <Footer />
+// </div>;
--- my-app/src/Componets/Navbar/Navbar.jsx
+++ my-app/src/Componets/Navbar/Navbar.jsx
@@ -4,179 +4,186 @@ import { GiSittingDog } from "react-icons/gi";
 import { NavLink } from "react-router-dom";
 
 import {
-	Box,
-	Flex,
-	Avatar,
-	HStack,
-	// Link,
-	IconButton,
-	Button,
-	Menu,
-	MenuButton,
-	MenuList,
-	MenuItem,
-	MenuDivider,
-	useDisclosure,
-	useColorModeValue,
-	Stack,
-	Image,
-	Text,
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  // Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Image,
+  Text,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
 export default function Simple() {
-	const { isOpen, onOpen, onClose } = useDisclosure();
-	const { user, isAuthenticated } = useAuth0();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { user, isAuthenticated } = useAuth0();
 
-	return (
-		<>
-			<Box bg="brand.green.100" px={100}>
-				<Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-					<IconButton
-						size={"md"}
-						icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-						aria-label={"Open Menu"}
-						display={{ md: "none" }}
-						onClick={isOpen ? onClose : onOpen}
-					/>
-					<HStack spacing={8} alignItems={"center"}>
-						<Box>
-							<NavLink to="/home">
-								<Image
-									src={logo}
-									// alt="Dan Abramov"
-									boxSize="70px"
-									mx="2rem"
-									mt="1rem"
-								/>
-							</NavLink>
-						</Box>
+  return (
+    <>
+      <Box bg="brand.green.100" px={100}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>
+              <NavLink to="/home">
+                <Image
+                  src={logo}
+                  // alt="Dan Abramov"
+                  boxSize="70px"
+                  mx="2rem"
+                  mt="1rem"
+                />
+              </NavLink>
+            </Box>
 
-						<HStack
-							as={"nav"}
-							spacing={7}
-							display={{ base: "none", md: "flex" }}>
-							<NavLink
-								px={2}
-								py={1}
-								rounded={"md"}
-								_hover={{
-									textDecoration: "none",
-									bg: useColorModeValue("gray.200", "gray.700"),
-								}}
-								to="/aboutUs"
-								variant="custom">
-								<Text fontFamily={"body"}>Sobre Nosotros</Text>
-							</NavLink>
-							<NavLink
-								px={2}
-								py={1}
-								rounded={"md"}
-								_hover={{
-									textDecoration: "none",
-									bg: useColorModeValue("gray.200", "gray.700"),
-								}}
-								to="/donate"
-								variant="custom">
-								<Text fontFamily={"body"}>Donaciones</Text>
-							</NavLink>
-							<NavLink
-								px={2}
-								py={1}
-								rounded={"md"}
-								_hover={{
-									textDecoration: "none",
-									bg: useColorModeValue("gray.200", "gray.700"),
-								}}
-								to="/shop"
-								variant="custom">
-								<Text fontFamily={"body"}>Tienda</Text>
-							</NavLink>
-							<NavLink
-								px={2}
-								py={1}
-								rounded={"md"}
-								_hover={{
-									textDecoration: "none",
-									bg: useColorModeValue("gray.200", "gray.700"),
-								}}
-								to="/createPet"
-								variant="custom">
-								<Text fontFamily={"body"}>Publicar Mascota</Text>
-							</NavLink>
-							<NavLink
-								px={2}
-								py={1}
-								rounded={"md"}
-								_hover={{
-									textDecoration: "none",
-									bg: useColorModeValue("gray.200", "gray.700"),
-								}}
-								to="/veterinary"
-								variant="custom">
-								<Text fontFamily={"body"}>Veterinarias</Text>
-							</NavLink>
+            <HStack
+              as={"nav"}
+              spacing={7}
+              display={{ base: "none", md: "flex" }}
+            >
+              <NavLink
+                px={2}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                to="/aboutUs"
+                variant="custom"
+              >
+                <Text fontFamily={"body"}>Nosotros</Text>
+              </NavLink>
+              <NavLink
+                px={2}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                to="/donate"
+                variant="custom"
+              >
+                <Text fontFamily={"body"}>Donaciones</Text>
+              </NavLink>
+              <NavLink
+                px={2}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                to="/shop"
+                variant="custom"
+              >
+                <Text fontFamily={"body"}>Tienda</Text>
+              </NavLink>
+              <NavLink
+                px={2}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                to="/createPet"
+                variant="custom"
+              >
+                <Text fontFamily={"body"}>Publicar Mascota</Text>
+              </NavLink>
+              <NavLink
+                px={2}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                to="/veterinary"
+                variant="custom"
+              >
+                <Text fontFamily={"body"}>Veterinarias</Text>
+              </NavLink>
 
-							<Menu>
-								<MenuButton>
-									<GiSittingDog size="20px" />
-								</MenuButton>
-								<MenuList>
-									<MenuItem>
-										<NavLink to="/adoptions">
-											<Text fontFamily={"body"}>Adopcion</Text>
-										</NavLink>
-									</MenuItem>
-									<MenuItem>
-										<NavLink to="/lostPets">
-											<Text fontFamily={"body"}>Perdidos</Text>
-										</NavLink>
-									</MenuItem>
-								</MenuList>
-							</Menu>
-						</HStack>
-					</HStack>
-					<Flex alignItems={"center"}>
-						<Menu>
-							<MenuButton
-								as={Button}
-								rounded={"full"}
-								variant={"link"}
-								cursor={"pointer"}
-								minW={0}>
-								<Avatar
-									size={"sm"}
-									borderBlockEndColor={"brand.orange"}
-									src={
-										isAuthenticated
-											? user?.picture
-											: "https://st2.depositphotos.com/19428878/44645/v/450/depositphotos_446453832-stock-illustration-default-avatar-profile-icon-social.jpg"
-									}
-								/>
-							</MenuButton>
-							<MenuList>
-								<MenuItem>Perfil</MenuItem>
-								<MenuItem>Mis mascotas</MenuItem>
+              <Menu>
+                <MenuButton>
+                  <GiSittingDog size="20px" />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>
+                    <NavLink to="/adoptions">
+                      <Text fontFamily={"body"}>Adopcion</Text>
+                    </NavLink>
+                  </MenuItem>
+                  <MenuItem>
+                    <NavLink to="/lostPets">
+                      <Text fontFamily={"body"}>Perdidos</Text>
+                    </NavLink>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
+                minW={0}
+              >
+                <Avatar
+                  size={"sm"}
+                  borderBlockEndColor={"brand.orange"}
+                  src={
+                    isAuthenticated
+                      ? user?.picture
+                      : "https://st2.depositphotos.com/19428878/44645/v/450/depositphotos_446453832-stock-illustration-default-avatar-profile-icon-social.jpg"
+                  }
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Perfil</MenuItem>
+                <MenuItem>Mis mascotas</MenuItem>
 
-								<MenuDivider />
-								<MenuItem>
-									<NavLink to="/">Ingresar</NavLink>
-								</MenuItem>
-							</MenuList>
-						</Menu>
-					</Flex>
-				</Flex>
+                <MenuDivider />
+                <MenuItem>
+                  <NavLink to="/">Ingresar</NavLink>
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
 
-				{isOpen ? (
-					<Box pb={4} display={{ md: "none" }}>
-						<Stack as={"nav"} spacing={4}>
-							{/* {Links.map((link) => (
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {/* {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
               ))} */}
-						</Stack>
-					</Box>
-				) : null}
-			</Box>
-		</>
-	);
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
 }
