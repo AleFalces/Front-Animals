04963ce3f96636c90040e35ab4812b866b0d41f3
coderman47 <askinglucho@gmail.com>
2023-01-30 22:56:45 -0300

    Create: components/buttons for show data. Lucho

--- my-app/src/Componets/DashboardAdmin/Dashboard/Dashboard.jsx
+++ my-app/src/Componets/DashboardAdmin/Dashboard/Dashboard.jsx
@@ -1,13 +1,62 @@
 import React from "react";
-import {styles} from './Dashboard.module.css'
+import { getAllProducts, getAllUsers, getAllPets } from "../../../Redux/Actions";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ShowUsers from "./ShowUsers";
+import ShowPets from "./ShowPets";
+
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
 
-    return (
-        <div>
-            <p>Dashboard</p>
-        </div>
-    )
-}
+  const [selection, setSelection] = useState("");
 
-export default Dashboard
+  function handlerShowDataUsers(e) {
+    e.preventDefault();
+    setSelection("users");
+  }
+  
+  function handlerShowDataPets(e) {
+    e.preventDefault();
+    setSelection("pets");
+  }
+  
+  function handlerShowDataProduct(e) {
+    e.preventDefault();
+    setSelection("product");
+  }
+
+  useEffect(() => {
+    dispatch(getAllUsers());
+    dispatch(getAllPets())
+    dispatch(getAllProducts())
+  }, [dispatch]);
+
+  return (
+    <div>
+      <div>
+        <button onClick={(e) => handlerShowDataUsers(e)}>Ver usuarios</button>
+        <button onClick={(e) => handlerShowDataPets(e)}>Ver mascotas</button>
+        {/* <button value="products">Product List</button> */}
+      </div>
+      <div>
+        {selection === "users" ? (
+          <ShowUsers/>
+        ) : selection === "products" ? (
+          <>"products"</>
+        ) : selection === "pets" ? (
+          <ShowPets/>
+        ) : (
+          <>"Elegir una de las opciones"</>
+        )}
+      </div>
+    </div>
+  );
+};
+
+//componente para agregar, quitar o editar productos de la tienda
+//componente para cargar imagenes
+//componente para controlar usuarios: bloquear o desbloqeuar
+//componente de solicitudes de cambios generados por el usuario
+
+export default Dashboard;
