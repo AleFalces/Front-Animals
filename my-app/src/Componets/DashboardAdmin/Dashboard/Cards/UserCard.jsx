ff903e425ca8f62310d44a447b202ecda5d62d06
coderman47 <askinglucho@gmail.com>
2023-01-31 18:06:14 -0300

    Create: components cards for show data in admin panel && pass token to dashboard routes

---
+++ my-app/src/Componets/DashboardAdmin/Dashboard/Cards/UserCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Card} from "@chakra-ui/react"
+
+const UserCard = ({ data: { size, img, sex, species, age } }) => {
+    return (
+      <div >
+        
+      </div>
+    );
+  };
+
+export default UserCard
