30691f3adaa9f14a15309421767ad20ccc1c85a8
FloatingRiver8 <maceiramariana@gmail.com>
2023-02-06 17:32:11 -0300

    form navbar s

--- my-app/src/Componets/DashboardAdmin/TopNav/TopNav.jsx
+++ my-app/src/Componets/DashboardAdmin/TopNav/TopNav.jsx
@@ -8,86 +8,83 @@ import { MdArrowBackIosNew } from 'react-icons/md';
 
 const TopNav = () => {
   return (
-    <nav className={styles.topNav}>
-      <h5 className={styles.brand}>Administrar sitio Web</h5>
 
-      <Link to={"/home"}>
-        <Icon
-          as={MdArrowBackIosNew}
-          color="orange.400"
-          boxSize={5}
-          _hover={{
-            color: "grey",
-            boxSize: "7",
-          }}
-        />
-        <Icon
-          as={MdArrowBackIosNew}
-          color="orange.400"
-          boxSize={5}
-          _hover={{
-            color: "grey",
-            boxSize: "7",
-          }}
-        />
-        <Button
-          fontFamily={"body"}
-          bg="base.green.100"
-          color={"grey"}
-          _hover={{
-            color: "orange.400",
-          }}
-          p="0"
-          mr="1rem">
-          {" "}
-          Atrás
-        </Button>
-      </Link>
+      <nav /* className={styles.topNav} */>
+        <SimpleGrid columns={[2, 4]} pt='2rem' bg={"brand.green.100"} pb={'1rem'}>
+          <Box>
+            <h5 className={styles.brand}>Administrar sitio Web</h5>
+          </Box>
 
 
 
+          <Box>
+            <Link to={"/home"}>
+              <Icon
+                as={MdArrowBackIosNew}
+                color="orange.400"
+                boxSize={5}
+                _hover={{
+                  color: "grey",
+                  boxSize: "7",
+                }}
+              />
+              <Icon
+                as={MdArrowBackIosNew}
+                color="orange.400"
+                boxSize={5}
+                _hover={{
+                  color: "grey",
+                  boxSize: "7",
+                }}
+              />
+              <Button
+                fontFamily={"body"}
+                bg="base.green.100"
+                color={"grey"}
+                _hover={{
+                  color: "orange.400",
+                }}
+                p="0"
+                mr="1rem">
+                {" "}
+                Atrás
+              </Button>
+            </Link>
+          </Box>
 
+          <Box>
+            <Link to={"/dashboard/createProduct"}>
+              <Button
+                loadingText="Publicar el producto"
+                size="lg"
+                bg={"brand.green.300"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >Publicar producto
+              </Button>
+            </Link>
+          </Box>
 
+          <Box>
+            <Link to={"/dashboard/createVet"}>
+              <Button
+                loadingText="Afiliar Veterinaria"
+                size="lg"
+                bg={"brand.green.300"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >Afiliar Veterinaria
+              </Button>
+            </Link>
+          </Box>
+        </SimpleGrid>
+      </nav>
 
-
-
-
-
-
-
-
-      <Button
-        loadingText="Publicar el producto"
-        size="lg"
-        bg={"blue.400"}
-        color={"white"}
-        _hover={{
-          bg: "blue.500",
-        }}
-      >
-        <Box>
-          <Link to={"/dashboard/createProduct"}>
-            <button>Publicar producto </button>
-          </Link>
-        </Box>
-      </Button>
-      <Button
-        loadingText="Afiliar Veterinaria"
-        size="lg"
-        bg={"blue.400"}
-        color={"white"}
-        _hover={{
-          bg: "blue.500",
-        }}
-      >
-        <Box>
-          <Link to={"/dashboard/createVet"}>
-            <button>Afiliar Veterinaria </button>
-          </Link>
-        </Box>
-      </Button>
-    </nav>
-  );
+      );
 };
 
-export default TopNav;
+      export default TopNav;
--- my-app/src/Componets/FormPostUser/FormPostUser.jsx
+++ my-app/src/Componets/FormPostUser/FormPostUser.jsx
@@ -1,9 +1,14 @@
+
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { postUser } from "../../Redux/Actions";
+import { ErrorForm, SuccedForm } from "../FormPostPet/AlertForm/AlertForm";
+import { MdArrowBackIosNew } from "react-icons/md";
+import logo from '../../assets/imagenes/logo_negro.png'
 import {
+
   Flex,
   Box,
   FormControl,
@@ -14,13 +19,64 @@ import {
   Button,
   Heading,
   Text,
-  //   useColorModeValue,
+  InputGroup,
+  InputRightElement,
+  InputLeftAddon,
+  Icon, Image
+
 } from "@chakra-ui/react";
+
+
+//Regular expressions for mail
+let isEmail = new RegExp('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+)
+
+
+const validateForm = (input) => {
+  let inputError = {}
+
+  if (input.name.trim() === "" || !input.name.length) {
+    inputError.name = "Debes ingresar tu nombre";
+  }
+  if (input.surname === "") {
+    inputError.surname = `Debes ingresar tu apellido`;
+  }
+  if (input.email === "" || !input.name.length) {
+    inputError.email = "Debes ingresar tu e-mail";
+  } else if (
+    !isEmail.test(input.email)) {
+    inputError.email = "ingresa formato de e-mail válido"
+  }
+  if (input.username === "") {
+    inputError.username = "Debes ingresar un nombre de usuario";
+  }
+  if (input.phone !== "") {
+    if (input.phone.length !== 10) {
+      inputError.phone = "Debe ser un numero de 10 digitos";
+    }
+
+  }
+  return inputError;
+}
+
 
 export default function FormPostUser({ value }) {
   const dispatch = useDispatch();
 
-  useEffect(() => {}, []);
+  useEffect(() => { }, []);
+
+  //Display login feedback
+  const [isIncomplete, setIsIncomplete] = useState(false);
+  const [infoSend, setInfoSend] = useState(false);
+
+
+  //Show password
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+
+
+  const [inputError, setInputError] = useState({});
   const [input, setInput] = useState({
     name: "",
     surname: "",
@@ -29,45 +85,7 @@ export default function FormPostUser({ value }) {
     phone: "",
     role: "user",
   });
-  const errors = {
-    name: "",
-    surname: "",
-    email: "",
-    username: "",
-    phone: "",
-  };
 
-  function handlerErrors(e) {
-    e.preventDefault();
-    if (input.name === "") {
-      errors.name = "Debes ingresar tu nombre";
-    }
-    if (input.surname === "") {
-      errors.surname = `Debes ingresar tu apellido`;
-    }
-    if (input.email === "") {
-      errors.email = "Debes ingresar tu e-mail";
-    }
-    if (input.username === "") {
-      errors.username = "Debes ingresar un nombre de usuario";
-    }
-    if (input.phone !== "") {
-      if (input.phone.length !== 8) {
-        errors.phone = "Debe ser un numero de 10 digitos";
-      }
-    }
-    if (
-      !errors.name &&
-      !errors.surname &&
-      !errors.email &&
-      !errors.username &&
-      !errors.phone
-    ) {
-      handlerSubmit(e);
-    } else {
-      alert("Falta rellenar algun campo");
-    }
-  }
 
   const handlerChange = (e) => {
     setInput({
@@ -76,151 +94,264 @@ export default function FormPostUser({ value }) {
     });
     console.log("input", input);
     // console.log("error", errors);
+
+    //control errores
+    setInputError(
+      validateForm({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
   };
+  console.log("INPUT FORM", input);
+
 
   const handlerSubmit = (e) => {
     e.preventDefault();
+    if (
+      input.name &&
+      input.surname &&
+      input.email &&
+      input.username &&
+      input.phone
+    ) {
+      /* handlerSubmit(e); */
+      dispatch(postUser(input));
 
-    dispatch(postUser(input));
-    alert("Usuario creado con éxito!");
+      setIsIncomplete(false);
+      setInfoSend(true);
+
+
+      //borra todos los inputs pero no sé cómo será con el tema del form con put que trae info a rellenar, creo , asiq queda comentada!
+      /*   document.getElementById("myForm").reset(); */
+
+    } else {
+
+      setIsIncomplete(true);
+      setInfoSend(false);
+
+    }
+
   };
 
+
+
+
+
   return (
-    <form>
-      <Flex
-        minH={"100vh"}
-        align={"center"}
-        justify={"center"}
-        //   bg={useColorModeValue("gray.50", "gray.800")}
-      >
-        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-          {value === undefined ? (
-            <Stack align={"center"}>
-              <Heading fontSize={"4xl"} textAlign={"center"}>
-                Crear cuenta en Buddy
-              </Heading>
-            </Stack>
-          ) : (
-            <Stack align={"center"}>
-              <Heading fontSize={"4xl"} textAlign={"center"}>
-                Actualizá la información de perfil
-              </Heading>
-            </Stack>
-          )}
+    <div>
+      {isIncomplete ? <ErrorForm /> : null}
+      {infoSend ? <SuccedForm /> : null}
 
-          <Box
-            rounded={"lg"}
-            //   bg={useColorModeValue("white", "gray.700")}
-            boxShadow={"lg"}
-            p={8}
-          >
-            <Stack spacing={4}>
-              <HStack>
-                <Box>
-                  <FormControl id="name" isRequired>
-                    <FormLabel>Nombre: </FormLabel>
-                    <Input
-                      type="text"
-                      name="name"
-                      key="name"
-                      //   value={input.name}
-                      onChange={(e) => handlerChange(e)}
-                    />
-                    {errors.name && <Text>{errors.name}</Text>}
-                  </FormControl>
+      <form id="myForm">
+        <Flex
+          minH={"100vh"}
+          align={"center"}
+          justify={"center"}
+          bg="brand.green.200"
+        >
+          <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+            {value === undefined ? (
+              <Stack align={"center"}>
+                <Text fontFamily="heading" fontWeight={'bold'} color='gray.600' fontSize={"5xl"} textAlign={"center"} textShadow={''}>
+                  Crea tu cuenta!
+                </Text>
+                <Text fontSize={"lg"} color={"gray.600"}>
+                  Gracias por cuidar a los animales ✌️
+                </Text>
+                <Box width={20} height={20}>
+                  <Image src={logo} ></Image>
                 </Box>
-
-                <Box>
-                  <FormControl id="surname" isRequired>
-                    <FormLabel>Apellido: </FormLabel>
-                    <Input
-                      name="surname"
-                      type="text"
-                      key="surname"
-                      onChange={(e) => handlerChange(e)}
-                    />
-                  </FormControl>
-                </Box>
-              </HStack>
-
-              <FormControl id="username" isRequired>
-                <FormLabel>Apodo: </FormLabel>
-                <Input
-                  name="username"
-                  value={input.description}
-                  key="username"
-                  type="text"
-                  onChange={(e) => handlerChange(e)}
-                />
-              </FormControl>
-
-              <FormControl id="email" isRequired>
-                <FormLabel>Email: </FormLabel>
-                <Input
-                  name="email"
-                  type="text"
-                  value={input.email}
-                  key="email"
-                  onChange={(e) => handlerChange(e)}
-                ></Input>
-              </FormControl>
-
-              <FormControl id="password" isRequired>
-                <FormLabel>Contraseña: </FormLabel>
-                <Input
-                  placeholder="Ingresar una contraseña"
-                  name="password"
-                  key="password"
-                  type="text"
-                  onChange={(e) => handlerChange(e)}
-                />
-              </FormControl>
-
-              <FormControl id="phone" isRequired>
-                <FormLabel>Cel/Teléfono:</FormLabel>
-                <Input
-                  type="number"
-                  name="phone"
-                  value={input.phone}
-                  onChange={(e) => handlerChange(e)}
-                />
-              </FormControl>
-              <Stack spacing={10} pt={2}>
-                {value === undefined ? (
-                  <Button
-                    onClick={(e) => handlerErrors(e)}
-                    loadingText="Registrar usuario"
-                    size="lg"
-                    bg={"blue.400"}
-                    color={"white"}
-                    _hover={{
-                      bg: "blue.500",
-                    }}
-                  >
-                    Registrarse
-                  </Button>
-                ) : (
-                  <Button
-                    onClick={(e) => handlerErrors(e)}
-                    loadingText="Guardar cambios"
-                    size="lg"
-                    bg={"blue.400"}
-                    color={"white"}
-                    _hover={{
-                      bg: "blue.500",
-                    }}
-                  >
-                    Guardar cambios
-                  </Button>
-                )}
               </Stack>
-            </Stack>
-          </Box>
-          <Link to={"/"}>
-            <Button>Atrás</Button>
-          </Link>
-        </Stack>
-      </Flex>
-    </form>
+            ) : (
+              <Stack align={"center"}>
+                <Heading fontSize={"4xl"} textAlign={"center"}>
+                  Actualizá la información de perfil
+                </Heading>
+              </Stack>
+            )}
+
+            <Box
+              rounded={"lg"}
+              bg={"white"}
+              boxShadow={"lg"}
+              p={8}
+            >
+              <Stack spacing={4}>
+                <HStack>
+                  <Box>
+                    <FormControl id="name" isRequired>
+                      <FormLabel>Nombre: </FormLabel>
+                      <Input
+                        type="text"
+                        name="name"
+                        key="name"
+                        focusBorderColor={"brand.green.300"}
+                        fontFamily={"body"}
+                        // value={input.name} 
+                        onChange={(e) => handlerChange(e)}
+                      />
+                      {inputError.name && (<Text className="text_inputError">{inputError.name}</Text>)}
+                    </FormControl>
+
+                  </Box>
+
+                  <Box>
+                    <FormControl id="surname" isRequired>
+                      <FormLabel>Apellido: </FormLabel>
+                      <Input
+                        name="surname"
+                        type="text"
+                        key="surname"
+                        focusBorderColor={"brand.green.300"}
+                        fontFamily={"body"}
+                        onChange={(e) => handlerChange(e)}
+                      />
+                      {inputError.surname && (<Text className="text_inputError">{inputError.surname}</Text>)}
+                    </FormControl>
+                  </Box>
+                </HStack>
+
+                <FormControl id="username" isRequired>
+                  <FormLabel>Apodo: </FormLabel>
+                  <Input
+                    name="username"
+                    value={input.description}
+                    key="username"
+                    type="text"
+                    focusBorderColor={"brand.green.300"}
+                    fontFamily={"body"}
+                    onChange={(e) => handlerChange(e)}
+                  />{inputError.username && (<Text className="text_inputError">{inputError.username}</Text>)}
+                </FormControl>
+
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email: </FormLabel>
+                  <Input
+                    name="email"
+                    type="text"
+                    value={input.email}
+                    key="email"
+                    placeholder="tumail@mail.com"
+                    focusBorderColor={"brand.green.300"}
+                    fontFamily={"body"}
+                    onChange={(e) => handlerChange(e)}
+                  ></Input>
+                  {inputError.email && (<Text className="text_inputError">{inputError.email}</Text>)}
+                </FormControl>
+
+                <FormControl id="password" isRequired>
+                  <FormLabel>Contraseña: </FormLabel>
+                  <InputGroup size="md">
+                    <Input
+                      placeholder="Ingresar una contraseña"
+                      name="password"
+                      key="password"
+                      focusBorderColor={"brand.green.300"}
+                      fontFamily={"body"}
+                      type={show ? 'text' : 'password'}
+                      onChange={(e) => handlerChange(e)}
+                    />
+                    <InputRightElement width="4.5rem">
+                      <Button h="1.75rem" size="sm" onClick={handleClick}>
+                        {show ? 'Hide' : 'Show'}
+                      </Button>
+                    </InputRightElement>
+                    {inputError.password && (<Text>{inputError.password}</Text>)}
+                  </InputGroup>
+                </FormControl>
+
+                <FormControl id="phone" isRequired>
+                  <FormLabel>Cel/Teléfono:</FormLabel>
+                  <InputGroup size="sm">
+                    <InputLeftAddon bg="gray.50"
+                      _dark={{
+                        bg: 'gray.800',
+                      }}
+                      color="gray.500"
+                      rounded="md">
+                      +54 9
+                    </InputLeftAddon>
+                    <Input
+                      type="number"
+                      name="phone"
+                      value={input.phone}
+                      focusBorderColor={"brand.green.300"}
+                      fontFamily={"body"}
+                      onChange={(e) => handlerChange(e)}
+                    />
+                  </InputGroup>
+                  {inputError.phone && (<Text className="text_inputError">{inputError.phone}</Text>)}
+                </FormControl>
+                <Stack spacing={10} pt={2}>
+                  {value === undefined ? (
+                    <Button
+                      onClick={(e) => [handlerSubmit(e), window.scrollTo(0, 0)]}
+                      loadingText="Post mascota"
+                      fontFamily={"body"}
+                      size="lg"
+                      bg={"orange.300"}
+                      color={"white"}
+                      _hover={{
+                        bg: "orange.400",
+
+                      }}>
+                      Registrarse
+                    </Button>
+                  ) : (
+                    <Button
+                      onClick={(e) => [handlerSubmit(e), window.scrollTo(0, 0)]}
+                      loadingText="Post mascota"
+                      fontFamily={"body"}
+                      size="lg"
+                      bg={"orange.300"}
+                      color={"white"}
+                      _hover={{
+                        bg: "orange.400",
+
+                      }}>
+                      Guardar cambios
+                    </Button>
+                  )}
+                </Stack>
+              </Stack>
+            </Box>
+            <Link to={"/"}>
+              <Icon
+                as={MdArrowBackIosNew}
+                color="orange.400"
+                boxSize={5}
+                _hover={{
+                  color: "grey",
+                  boxSize: "7",
+                }}
+              />
+              <Icon
+                as={MdArrowBackIosNew}
+                color="orange.400"
+                boxSize={5}
+                _hover={{
+                  color: "grey",
+                  boxSize: "7",
+                }}
+              />
+              <Button
+                fontFamily={"body"}
+                bg="base.green.100"
+                color={"grey"}
+                _hover={{
+                  color: "orange.400",
+                }}
+                p="0"
+                mr="1rem">
+                {" "}
+                Atrás
+              </Button>
+            </Link>
+          </Stack>
+        </Flex>
+      </form>
+    </div>
   );
 }
+
