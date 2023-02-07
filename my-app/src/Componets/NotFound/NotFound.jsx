0cd7a5e7fd53c74c92aed7ff40ca2c7933940425
Tbownt <98657140+Tbownt@users.noreply.github.com>
2023-02-06 21:12:19 -0400

    componentes erroneos estilados

--- my-app/src/Componets/NotFound/NotFound.jsx
+++ my-app/src/Componets/NotFound/NotFound.jsx
@@ -1,44 +1,52 @@
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./NotFound.css";
 import img5 from "../../assets/imagenes/img5.png";
+import { Box, Image, Center, Heading, Button } from "@chakra-ui/react";
 
 const NotFound = () => {
-	const navigate = useNavigate();
-	// const redirect = () => setTimeout(() => navigate("/home"), 5000);
-
-	// useEffect(() => {
-	// 	redirect();
-	// }, []);
-
-	return (
-		<div>
-			<Navbar />
-			<div className="contenedor-notFound">
-				<h2 className="texto-NotFound">
-					Ups.. Ruta no encontrada, redirigiendo a Home en 5 segundos..
-				</h2>
-				<img className="d-block w-100 edit" src={img5} alt="First slide" />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-			</div>
-			<Footer />
-		</div>
-	);
+  const navigate = useNavigate();
+  return (
+    <Box minH="100vh" position="relative" bg="brand.green.200">
+      <Navbar />
+      <Box>
+        <Center>
+          <Heading
+            fontSize={{ base: "16px", sm: "2em" }}
+            py={10}
+            fontWeight={"bold"}
+            textAlign="center"
+            maxW="20em"
+            fontFamily={"heading"}
+            mt="1%"
+            mb="1%"
+          >
+            Lo sentimos...
+            <br /> No pudimos encontrar esa ruta
+          </Heading>
+        </Center>
+        <Center>
+          <Image
+            src={img5}
+            // w={{ base: "80%", sm: "50%", md: "45%" }}
+            size={{ base: "sm" }}
+            borderRadius="2xl"
+            mb="5%"
+          />
+        </Center>
+        <Button
+          colorScheme="red"
+          mb="5%"
+          size="sm"
+          onClick={() => navigate("/home")}
+        >
+          Volver atras
+        </Button>
+      </Box>
+      <Footer />
+    </Box>
+  );
 };
 
 export default NotFound;
