b7278758d9c820c583913fc2088457994bf1f0c5
maguialiaga <maguialiaga1@gmail.com>
2023-02-07 13:46:25 -0300

    productDetail

--- my-app/src/Componets/Shop/ProductDetail/ProductDetail.jsx
+++ my-app/src/Componets/Shop/ProductDetail/ProductDetail.jsx
@@ -23,78 +23,91 @@ export default function ProductDetail() {
 
   useEffect(() => {}, [dispatch]);
 
+  const payMp = async () => {
+    axios
+      .post(`http://localhost:3001/donation`, {
+        unit_price: detail[0].price,
+        title: detail[0].name,
+      })
+      .then((response) => {
+        window.open(response.data, "_blank");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  console.log("DETAIL PRODUCT STOCK :", detail[0].stock);
 
-	const payMp = async ()=>{
-		axios.post(`http://localhost:3001/donation`,
-		 {
-		  unit_price: detail[0].price,
-		  title : detail[0].name,
-		})
-		.then(response => {
-		  window.open(response.data, '_blank');
-		  })
-		  .catch(error => {
-		  console.error(error);
-		  });
-	  }
-console.log("DETAIL PRODUCT STOCK :", detail[0].stock);
-	  
-	return (
-		<div className="detailProductContainerAll">
-			<Navbar />
-			<div className="detailProductNavbarDivBottom"></div>
-			<div className="detailProductContainer">
-				<div className="divDetailProductDescription">
-					<div className="detailProductDescription">
-						<h1>{detail[0].name}</h1>
-						<h1>Puntuacion </h1>
-						<h4>★★★</h4>
-						<div className="detailDescription">
-							<div>
-								<h2>Categoria</h2> <h3>{detail[0].Category}</h3>
-							</div>
-							<div className="detailProductDescription2">
-								<h2>Descripcion:</h2> <h3>{detail[0].description}</h3>
-							</div>
-							<div>
-								<h2>Precio</h2> <h3>${detail[0].price}</h3>{" "}
-							</div>
-							<div><h2>Stock</h2> <h3>{detail[0].stock}u</h3></div>
-						</div>
-					</div>
-					<div className="imgxbutton">
-						<img src={detail[0].image} alt="" />
-						<div>
-              		<div className="footerProductDetail">
-			         <Footer />
-		</div>
-              
-              )}
+  return (
+    <>
+      return (
+      <div className="detailProductContainerAll">
+        <Navbar />
+        <div className="detailProductNavbarDivBottom"></div>
+        <div className="detailProductContainer">
+          <div className="divDetailProductDescription">
+            <div className="detailProductDescription">
+              <h1>{detail[0].name}</h1>
+              {/* <h1>Puntuacion </h1> */}
+              {/* <h4>★★★</h4> */}
+              <div className="detailDescription">
+                <div>
+                  <h2>Categoria</h2> <h3>{detail[0].Category}</h3>
+                </div>
+                <div className="detailProductDescription2">
+                  <h2>Descripcion:</h2> <h3>{detail[0].description}</h3>
+                </div>
+                <div>
+                  <h2>Precio</h2> <h3>${detail[0].price}</h3>{" "}
+                </div>
+                <div>
+                  <h2>Stock</h2> <h3>{detail[0].stock}u</h3>
+                </div>
+              </div>
+            </div>
+            <div className="imgxbutton">
+              <img src={detail[0].image} alt="" />
+              <div>
+                <button onClick={() => payMp()}>Comprar</button>
 
-					//		<button onClick={(e)=>detail[0].handlerSetCart(e, detail[0].id, detail[0].price, detail[0].image, detail[0].name, detail[0].stock )}>Agregar</button>
+                <button
+                  onClick={(e) =>
+                    detail[0].handlerSetCart(
+                      e,
+                      detail[0].id,
+                      detail[0].price,
+                      detail[0].image,
+                      detail[0].name,
+                      detail[0].stock
+                    )
+                  }
+                >
+                  Agregar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="divTopReview"></div>
+        <div className="reviewsContainer">Reseñas</div>
+        <div className="footerProductDetail">
+          <Footer />
+        </div>
+      </div>
+      );
+    </>
+  );
+}
 
-//   return (
-//     <>
-//       <Navbar />
-//       <Box
-//         minHeight={"150vh"}
-//         bg="brand.backgorund"
-//         paddingBottom={"3rem"}
-//       ></Box>
-//     </>
-//   );
-// }
+/* //		<button onClick={(e)=>detail[0].handlerSetCart(e, detail[0].id, detail[0].price, detail[0].image, detail[0].name, detail[0].stock )}>Agregar</button> */
+/* 
+						<button onClick={()=>payMp()}>Comprar</button>
 
+					<button onClick={(e)=>detail[0].handlerSetCart(e, detail[0].id, detail[0].price, detail[0].image, detail[0].name )}>Agregar</button>
 
-
-
-// 						<button onClick={()=>payMp()}>Comprar</button>
-
-// 						<button onClick={(e)=>detail[0].handlerSetCart(e, detail[0].id, detail[0].price, detail[0].image, detail[0].name )}>Agregar</button>
-
-// 					</div>
-// 				</div>
-// 			</div>
+			</div>
+          </div>
+        </div>
 // 		</div>
 // 		<div className="divTopReview"></div>
 // 		<div className="reviewsContainer">
@@ -122,4 +135,32 @@ console.log("DETAIL PRODUCT STOCK :", detail[0].stock);
 // 		</div>
 // 		<div className="footerProductDetail">
 // 			<Footer />
-// 		</div>
+// 		</div> */
+
+// <div className="detailProductContainerAll">
+// 			<Navbar />
+// 			<div className="detailProductNavbarDivBottom"></div>
+// 			<div className="detailProductContainer">
+// 				<div className="divDetailProductDescription">
+// 					<div className="detailProductDescription">
+// 						<h1>{detail[0].name}</h1>
+// 						<div className="detailDescription">
+// 							<div>
+// 								<h2>Categoria</h2> <h3>{detail[0].Category}</h3>
+// 							</div>
+// 							<div className="detailProductDescription2">
+// 								<h2>Descripcion:</h2> <h3>{detail[0].description}</h3>
+// 							</div>
+// 							<div>
+// 								<h2>Precio</h2> <h3>${detail[0].price}</h3>{" "}
+// 							</div>
+// 							<div><h2>Stock</h2> <h3>{detail[0].stock}u</h3></div>
+// 						</div>
+// 					</div>
+// 					<div className="imgxbutton">
+// 						<img src={detail[0].image} alt="" />
+// 						<div>
+//               		<div className="footerProductDetail">
+// 			         <Footer />
+//               </div>
+//               </div></div>
